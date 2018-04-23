9086bc3b689d95cd25fc51b7236f1c395c4ebb7d
qli31 <35783462+qli31@users.noreply.github.com>
2018-04-23 19:26:59 -0400

    added all the files

--- login.php
+++ login.php
@@ -1,129 +1,118 @@
 <?php
-
-// Initialize the session
-session_start();
-
-if (isset($_SESSION['username'])) {
-    header("location: index.php");
-    exit;
-}
-
 // Include config file
-require_once 'includes/config.php';
+require_once 'config.php';
+ 
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
+ 
 // Processing form data when form is submitted
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
     // Check if username is empty
-    if (empty(trim($_POST["username"]))) {
+    if(empty(trim($_POST["username"]))){
         $username_err = 'Please enter username.';
-    } else {
+    } else{
         $username = trim($_POST["username"]);
     }
-
+    
     // Check if password is empty
-    if (empty(trim($_POST['password']))) {
+    if(empty(trim($_POST['password']))){
         $password_err = 'Please enter your password.';
-    } else {
+    } else{
         $password = trim($_POST['password']);
     }
-
+    
     // Validate credentials
-    if (empty($username_err) && empty($password_err)) {
+    if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
         $sql = "SELECT username, password FROM users WHERE username = ?";
-        if ($stmt = $connect->prepare($sql)) {
+        
+        if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("s", $param_username);
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
             // Set parameters
             $param_username = $username;
+            
             // Attempt to execute the prepared statement
-            if ($stmt->execute()) {
+            if(mysqli_stmt_execute($stmt)){
                 // Store result
-                $stmt->store_result();
+                mysqli_stmt_store_result($stmt);
+                
                 // Check if username exists, if yes then verify password
-                if ($stmt->num_rows == 1) {
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    $stmt->bind_result($username, $hashed_password);
-                    if ($stmt->fetch()) {
-                        if (password_verify($password, $hashed_password)) {
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
                             /* Password is correct, so start a new session and
                             save the username to the session */
                             session_start();
-                            $_SESSION['username'] = $username;
-                            header("location: index.php");
-                        } else {
+                            $_SESSION['username'] = $username;      
+                            header("location: admin_data.php");
+                        } else{
                             // Display an error message if password is not valid
                             $password_err = 'The password you entered was not valid.';
                         }
                     }
-                } else {
+                } else{
                     // Display an error message if username doesn't exist
                     $username_err = 'No account found with that username.';
                 }
-            } else {
+            } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
+        
         // Close statement
-        $stmt->close();
+        mysqli_stmt_close($stmt);
     }
+    
     // Close connection
-    $connect->close();
+    mysqli_close($link);
 }
-
-require_once "includes/html-head.php";
-
 ?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>>Pizza Admin Login</title>
 
-    <body>
-        <?php
-require_once "includes/navbar.php";
-?>
-
-            <h2>Login</h2>
-
-            <p>Please fill in your credentials to login.</p>
-
-            <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
-
-                <div class="<?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-
-                    <label>Username</label>
-
-                    <input type="text" name="username" value="<?php echo $username; ?>">
-
-                    <span>
-                        <?php echo $username_err; ?>
-                    </span>
-
-                </div>
-
-                <div class="<?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-
-                    <label>Password</label>
-
-                    <input type="password" name="password">
-
-                    <span>
-                        <?php echo $password_err; ?>
-                    </span>
-
-                </div>
-
-                <div>
-
-                    <input type="submit" value="Login">
-
-                </div>
-
-                <p>Don't have an account?
-                    <a href="register.php">Sign up now</a>.</p>
-
-            </form>
-
-    </body>
-
-    </html>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Pizza Admin Login</h2>
+  <nav>
+    <ul>
+      <li><a href="index.php">Pizza</a></li>
+      <li><a href="survey.php">Survey</a></li>
+      <li><a href="login.php">Admin login</a></li>
+    </ul>  
+  </nav>
+        <p>Please fill in your yummy credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
