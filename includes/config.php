f9d14551d74dfbeeb0d903a1e3bd735ad1b6ea6f
Zubair <zubairabaziz@gmail.com>
2018-04-28 13:43:25 -0400

    Connect to database

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'urcscon3_medell');
-define('DB_PASSWORD', 'coffee1N');
-define('DB_NAME', 'urcscon3_medell');
-
-// Tests on local server
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'csc174');
-// define('DB_PASSWORD', 'csc174');
-// define('DB_NAME', 'csc174');
+define('DB_USERNAME', 'urcscon3_zabaziz');
+define('DB_PASSWORD', 'zabaziz1');
+define('DB_NAME', 'urcscon3_zabaziz');
 
 /* Connect to MySQL database */
 $connect = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
